f0d32a10db479245f84d516f3f239072cd0c9bc7
zerookrash <zerookrash@me.com>
2019-09-07 05:00:47 -0500

    Formulario de Cotizaciones Listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { PerfilComponent } from './conponents/perfil/perfil.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/conponents/cotizador/cotizador.component.ts
+++ src/app/conponents/cotizador/cotizador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cotizador',
@@ -7,7 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CotizadorComponent implements OnInit {
 
-  constructor() { }
+  cotizaForm = new FormGroup({
+    anio : new FormControl(''),
+    marca : new FormControl('Marca del Automovil'),
+    modelo : new FormControl(''),
+    tipo : new FormControl('Tipo de Automovil'),
+    clausulas : new FormControl('Clausulas de Limite de Velocidad')
+  });
+
+  valorCotiza = 0;
+  primaBase = 50000;
+
+  calculo() {
+    // tslint:disable-next-line: no-unused-expression
+    const tipo = this.cotizaForm.value.tipo;
+    const clausula = this.cotizaForm.value.clausulas;
+    const marca = this.cotizaForm.value.marca;
+
+    if ( marca === 'Ford' && tipo === '2 Puertas' && clausula === 'Tipo A' ) {
+      this.valorCotiza = this.primaBase + (this.primaBase * .10);
+    } else if ( marca === 'Ford' && tipo === '2 Puertas' && clausula === 'Tipo B') {
+      this.valorCotiza = this.primaBase + (this.primaBase * .15);
+    } else if ( marca === 'Ford' && tipo === '4 Puertas' && clausula === 'Tipo A' ) {
+      this.valorCotiza = this.primaBase + (this.primaBase * .05);
+    } else if ( marca === 'Ford' && tipo === '4 Puertas' && clausula === 'Tipo B' ) {
+      this.valorCotiza = this.primaBase + (this.primaBase * .10);
+    } else if ( marca === 'Toyota' && tipo === '2 Puertas' && clausula === 'Tipo A') {
+      this.valorCotiza = this.primaBase + (this.primaBase * .05 );
+    } else if ( marca === 'Toyota' && tipo === '2 Puertas' && clausula === 'Tipo B') {
+      this.valorCotiza = this.primaBase + (this.primaBase * .1);
+    } else if ( marca === 'Toyota' && tipo === '4 Puertas' && clausula === 'Tipo A') {
+      this.valorCotiza = this.primaBase + (this.primaBase * .05);
+    } else if ( marca === 'Toyota' && tipo === '4 Puertas' && clausula === 'Tipo B') {
+      this.valorCotiza = this.primaBase + (this.primaBase * .0);
+    } else if ( marca === 'Nissan' && tipo === '2 Puertas' && clausula === 'Tipo A') {
+      this.valorCotiza = this.primaBase + (this.primaBase * .15);
+    } else if ( marca === 'Nissan' && tipo === '2 Puertas' && clausula === 'Tipo B') {
+      this.valorCotiza = this.primaBase + ( this.primaBase * .2);
+    } else if ( marca === 'Nissan' && tipo === '4 Puertas' && clausula === 'Tipo A') {
+      this. valorCotiza = this.primaBase + (this.primaBase * .1);
+    } else if ( marca === 'Nissan' && tipo === '4 Puertas' && clausula === 'Tipo B') {
+      this.valorCotiza = this.primaBase + (this.primaBase * .15);
+    }
+  }
+
+
+  constructor() {}
 
   ngOnInit() {
   }
